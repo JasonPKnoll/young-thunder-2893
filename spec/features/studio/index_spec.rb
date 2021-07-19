f846cbe8f6c3f8018540f566df4bcc46b621a99a
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-19 08:38:40 -0700

    Update gems and fix stuido spec test

--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -1,17 +1,18 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe 'the studio index' do
   before(:each) do
-    @studio = Studio.create!(:name "Universal Studios", location: "HollyWood")
-    @studio2 = Studio.create!(:name "Disney Pixar", location: "HollyWood")
-    @movie = studio.movies.create!(:title "The Mummy", :creation_year 1999, :genre "Horror")
-    @movie2 = studio.movies.create!(:title "Jaws", :creation_year 1975, :genre "Adventure")
-    @movie3 = studio.movies.create!(:title "Finding Nemo", :creation_year 2003, :genre "Adventure")
-    @movie4 = studio.movies.create!(:title "Up", :creation_year 2009, :genre "Adventure")
+    @studio = Studio.create!(name: "Universal Studios", location: "HollyWood")
+    @studio2 = Studio.create!(name: "Disney Pixar", location: "HollyWood")
+    @movie = @studio.movies.create!(title: "The Mummy", creation_year: 1999, genre: "Horror")
+    @movie2 = @studio.movies.create!(title: "Jaws", creation_year: 1975, genre: "Adventure")
+    @movie3 = @studio2.movies.create!(title: "Finding Nemo", creation_year: 2003, genre: "Adventure")
+    @movie4 = @studio2.movies.create!(title: "Up", creation_year: 2009, genre: "Adventure")
   end
 
   it 'displays studio and its movies' do
     visit "/studios"
+    save_and_open_page
 
     expect(page).to have_content("Universal Studios")
     expect(page).to have_content("The Mummy")
