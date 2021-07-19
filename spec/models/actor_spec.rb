ccabd278238a1cf5bd605baa20a7ad9478031336
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-19 10:52:06 -0700

    Add model test for actor sort

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Actor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:movie) }
+  end
+
+  before(:each) do
+    @actor = @movie.actors.create!(name: "Brendan Fraser", age: 31)
+    @actor2 = @movie.actors.create!(name: "Rachel Weisz", age: 29)
+    @actor3 = @movie.actors.create!(name: "Arnold Vosloo", age: 37)
+  end
+
+  it "can show actors by order from age" do
+    expect(Actor.sort_by_age).to eq([@actor2, @actor, @actor3])
+
+  end
 end
