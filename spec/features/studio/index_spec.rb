26bf012068bd4408392484dec9a73d453f16d061
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-19 08:29:07 -0700

    Add studio index spec tests

---
+++ spec/features/studio/index_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe 'the studio index' do
+  before(:each) do
+    @studio = Studio.create!(:name "Universal Studios", location: "HollyWood")
+    @studio2 = Studio.create!(:name "Disney Pixar", location: "HollyWood")
+    @movie = studio.movies.create!(:title "The Mummy", :creation_year 1999, :genre "Horror")
+    @movie2 = studio.movies.create!(:title "Jaws", :creation_year 1975, :genre "Adventure")
+    @movie3 = studio.movies.create!(:title "Finding Nemo", :creation_year 2003, :genre "Adventure")
+    @movie4 = studio.movies.create!(:title "Up", :creation_year 2009, :genre "Adventure")
+  end
+
+  it 'displays studio and its movies' do
+    visit "/studios"
+
+    expect(page).to have_content("Universal Studios")
+    expect(page).to have_content("The Mummy")
+    expect(page).to have_content("Jaws")
+    expect(page).to have_content("Disney Pixar")
+    expect(page).to have_content("Jaws")
+    expect(page).to have_content("Up")
+  end
+end
